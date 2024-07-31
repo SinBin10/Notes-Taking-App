0fe7ff668b740c2fb1ac388e18ec777057e2302a
SinBin10 <bincode10@gmail.com>
2024-07-31 18:50:33 +0530

    dynamic routing and integrating tailwind css.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/:username/:age", (req, res) => {
+  res.send(
+    `Your name is ${req.params.username} and your age is ${req.params.age}`
+  );
 });
 
 app.listen(3000);
