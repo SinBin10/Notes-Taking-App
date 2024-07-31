0986fb235dd1789bfe9247efc5b776323ec40f49
SinBin10 <bincode10@gmail.com>
2024-07-31 18:09:25 +0530

    setting up ejs

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Dispying...");
+  res.render("index");
 });
 
 app.listen(3000);
