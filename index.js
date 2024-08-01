002b0389bb8f6bc239f1e885e16310c8a5e84ea9
SinBin10 <bincode10@gmail.com>
2024-08-01 22:56:50 +0530

    using readdir to display number of tasks and displaying it on the webpage.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const fs = require("fs");
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -8,7 +9,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  fs.readdir("./files", (err, files) => {
+    res.render("index", { files: files });
+  });
 });
 
 app.get("/:username/:age", (req, res) => {
